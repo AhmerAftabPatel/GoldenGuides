5043677345f63d8f23ad0694223d99fe547701ec
AhmerAftabPatel <69456880+AhmerAftabPatel@users.noreply.github.com>
2021-05-22 18:31:53 +0530

    Delete client directory

--- client/src/components/AxiosHeaders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-export default function setAxiosDefaultHeaders() {
-    const API = "http://localhost:8700/"
-  axios.defaults.headers.post['Content-Type'] = 'application/json';
-  axios.defaults.baseURL = API;
-  axios.defaults.headers.post.Accept = 'application/json';
-}
